c3bcab4b90452980fd7f37afd1ad4c168cf6c9b7
Shadow <ShadowDevAt42@pm.me>
2024-09-13 21:41:51 +0200

    ex11

---
+++ ex11/ft_div_mod.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdi-tria <fdi-tria@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/13 21:38:06 by fdi-tria          #+#    #+#             */
+/*   Updated: 2024/09/13 21:40:51 by fdi-tria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	if (b != 0)
+	{
+		*div = a / b;
+		*mod = a % b;
+	}
+}
+
+/*#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod);
+
+int	main(void)
+{
+	int a = 42;
+	int b = 10;
+	int div;
+	int mod;
+
+	ft_div_mod(a, b, &div, &mod);
+	printf("a = %d, b = %d\n", a, b);
+	printf("Division : %d / %d = %d\n", a, b, div);
+	printf("Modulo : %d %% %d = %d\n", a, b, mod);
+	return (0);
+}*/
