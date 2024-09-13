4aeb21033e0e65052b3d489be6a233884ee0c850
Shadow <ShadowDevAt42@pm.me>
2024-09-13 21:36:54 +0200

    ex10

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -6,7 +6,32 @@
 /*   By: fdi-tria <fdi-tria@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 21:20:45 by fdi-tria          #+#    #+#             */
-/*   Updated: 2024/09/13 21:21:24 by fdi-tria         ###   ########.fr       */
+/*   Updated: 2024/09/13 21:36:01 by fdi-tria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*#include <stdio.h>
+
+void	ft_swap(int *a, int *b);
+
+int	main(void)
+{
+	int x;
+	int y;
+
+	x = 42;
+	y = 21;
+	printf("Avant : x = %d, y = %d\n", x, y);
+	ft_swap(&x, &y);  // Appel de la fonction pour échanger les valeurs de x et y
+	printf("Après : x = %d, y = %d\n", x, y);
+	return (0);
+}*/
